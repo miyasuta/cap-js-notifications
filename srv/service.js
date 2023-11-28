410a929fc9a1a038a43c5a0edda393b60bf3fb50
Mio Yasutake <mio.fujita.01@gmail.com>
2023-11-28 21:12:27 +0000

    send notification

--- srv/service.js
+++ srv/service.js
@@ -3,14 +3,14 @@ const cds = require('@sap/cds')
 module.exports = class NotificationService extends cds.ApplicationService {
     async init() {
         this.on('notify', async(req)=> {
-            // const alert = await cds.connect.to('notifications');
+            const alert = await cds.connect.to('notifications');
 
-            // alert.notify({
-            //     recipients: [ req.data.recipient ],
-            //     priority: "HIGH", //optional
-            //     title: "New high priority incident is assigned to you!",
-            //     description: "Incident titled 'Engine overheating' created by 'customer X' with priority high is assigned to you!"
-            //   });
+            alert.notify({
+                recipients: [ req.data.recipient ],
+                priority: "HIGH", //optional
+                title: "New high priority incident is assigned to you!",
+                description: "Incident titled 'Engine overheating' created by 'customer X' with priority high is assigned to you!"
+              });
 
         })
         return super.init()
